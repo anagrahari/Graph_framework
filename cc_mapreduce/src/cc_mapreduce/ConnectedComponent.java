8a437db4e91318740e5ec54189453ff5bb8c8b64
cloudera <cloudera@quickstart.cloudera>
2017-04-28 22:53:38 -0700

    cc

--- cc_mapreduce/src/cc_mapreduce/ConnectedComponent.java
+++ cc_mapreduce/src/cc_mapreduce/ConnectedComponent.java
@@ -25,6 +25,12 @@ class Vertex {
 		this.vId = vId;
 		this.neighbors = neighbors;
 	}
+	
+	public Vertex(int tag, long group) {
+		this.tag = tag;
+		this.group = group;
+	}
+	
 	int tag;
 	long group;
 	long vId;
@@ -34,8 +40,8 @@ class Vertex {
 public class ConnectedComponent {
 	public static class MapperFirstStage extends Mapper<Long, Text, Long, Vertex> {
 		
-		public void map(LongWritable key, Text value, Context con) throws IOException, InterruptedException {
-			String line = value.toString();
+		public void map(Long key, Text valueIn, Context con) throws IOException, InterruptedException {
+			String line = valueIn.toString();
 			String[] vertices =  line.split("");
 			long vertexId =  Long.parseLong(vertices[0]);
 			
@@ -44,23 +50,58 @@ public class ConnectedComponent {
 				neighbors.add(Long.parseLong(vertices[i]));
 			}
 			con.write(vertexId, new Vertex(0, vertexId, vertexId, neighbors));
+		}	
+	
+	}
+	
+	public static class MapperSecondStage extends Mapper<Long, Vertex, Long, Vertex> {
+		
+		public void map(Long key, Vertex inVertex, Context con) throws IOException, InterruptedException {
+			con.write(inVertex.vId, inVertex);
+			for(long vertexId : inVertex.neighbors) {
+				con.write(vertexId, new Vertex(1, inVertex.group));
+			}
+		}	
+		
+	}
+	
+	public static class ReducerSecondStage extends Mapper<Long, Vector<Vertex>, Long, Vertex> {
+		
+		@SuppressWarnings("unchecked")
+		public void reduce(Long vertexId, Vector<Vertex> values, Context con) throws IOException, InterruptedException {
+			Long M = Long.MAX_VALUE;
+			Object clone = null;
+			
+			for(Vertex v : values) {
+				if (v.tag == 0) {
+					clone = v.neighbors.clone();					
+				}
+				M = Math.min(M, v.group);
+				//con.write(M, new Vertex(0, M, vertexId, (Vector<Long>) clone));
+			}
+			con.write(M, new Vertex(0, M, vertexId, (Vector<Long>) clone));
+		}
+	}
+	
+	
+	public static class MapperFinalStage  extends Mapper <Long, Long, Long, Long> {
+		
+		public void map(Long group,  Long value, Context con) throws IOException, InterruptedException {
+			con.write(group, 1L);
 		}
 		
-	
 	}
 	
-	
-	
-	public static class MapperSecondStage extends Mapper<LongWritable, Text, Text, IntWritable> {
+	public static class ReducerFinalStage extends Reducer<Long, Vector<Long>, Long, Long> {
 		
-	}
-	
-	public static class ReducerSecondStage extends Mapper<LongWritable, Text, Text, IntWritable> {
-		
-	}
-	
-	
-	public static class MapperFinalStage  extends Mapper <LongWritable, Text, Text, IntWritable> {
+		public void reduce(Long group, Vector<Long> values, Context con) throws IOException, InterruptedException {
+			Long m = 0L;
+			
+			for(Long v : values) {
+				m = m + v;
+			}
+			con.write(group, m);
+		}
 		
 	}
 	
